8f65b5e77fb258747d7af14784b3c48f88289910
Denis <denislee56@gmail.com>
2026-02-05 12:24:14 +0500

    fix: specifications in model to embedded instead array

--- backend/src/model/laptop-model.js
+++ backend/src/model/laptop-model.js
@@ -4,7 +4,7 @@ const laptops_schema = mongoose.Schema({
 
     model_name : {type : String, required :true},
     price : {type : Number, required : true},
-    specifications : {type : [{ cpu : {type : String, required : true}, ram : {type : String, required : true}, storage : {type : String, required : true}, gpu : {type : String, required : true}}], required : true},
+    specifications : { cpu : {type : String, required : true}, ram : {type : Number, required : true}, storage : {type : String, required : true}, gpu : {type : String, required : true}},
     stock_quantity : {type : Number, required : true},
     brand_id : {type : mongoose.Types.ObjectId, required : true, ref : "Brands"}
 })
